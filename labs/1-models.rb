17862d820bf8f2fd482007068bd37eae03f35ca5
MeenalGakhar <Meenal.gakhar@kellogg.northwestern.edu>
2022-02-01 17:13:38 +0000

    more changes in class

--- labs/1-models.rb
+++ labs/1-models.rb
@@ -13,7 +13,54 @@ Contact.destroy_all
 
 # 2. create 1-2 new contacts for each company (they can be made up)
 
+amazon=Company.where({name:"Amazon"})[0]
+amazon_id = amazon.id
+new_contact =  Contact.new
+new_contact.first_name= "Andy"
+new_contact.last_name="Jassy"
+new_contact.email= "andy@amazon.com"
+new_contact.company_id= amazon_id
+new_contact.save
+puts new_contact.inspect
+puts "There are now #{Contact.all.count} contacts."
+
+tesla=Company.where({name:"Tesla"})[0]
+tesla_id = tesla.id
+new_contact =  Contact.new
+new_contact.first_name= "Elon"
+new_contact.last_name="Musk"
+new_contact.email= "elon@tesla.com"
+new_contact.company_id= tesla_id
+new_contact.save
+puts new_contact.inspect
+
+apple=Company.where({name:"Apple, Inc."})[0]
+apple_id = apple.id
+new_contact =  Contact.new
+new_contact.first_name= "Tim"
+new_contact.last_name="Cook"
+new_contact.email= "time@apple.com"
+new_contact.company_id= apple_id
+new_contact.save
+puts new_contact.inspect
+
+apple=Company.where({name:"Apple, Inc."})[0]
+apple_id = apple.id
+new_contact =  Contact.new
+new_contact.first_name= "Cgaig"
+new_contact.last_name="Fred"
+new_contact.email= "craig@apple.com"
+new_contact.company_id= apple_id
+new_contact.save
+puts new_contact.inspect
+
+
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
+puts "There are now #{Contact.all.count} contacts."
+all_contacts= Contact.all
+for contact in all_contacts
+    puts "#{contact.first_name} #{contact.last_name} #{contact.email}"
+end
 
 # ---------------------------------
 # Contacts: 4
--- labs/2-associations.rb
+++ labs/2-associations.rb
@@ -4,7 +4,31 @@
 # rails runner labs/2-associations.rb
 
 # 1. write code to display each contact (you can reuse the previous lab's code) and include the contact's company name, e.g.:
+companies= Company.all
+for company in companies
+    puts company.name
+    for contact in company.contacts
+        puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+    end
+end
 
+apple = Company.where({name: "Apple, Inc."})[0]
+puts "Apple, Inc"
+for contact in apple.contacts
+    puts"#{contact.first_name} #{contact.last_name} #{contact.email}"
+end
+
+amazon = Company.where({name: "Amazon"})[0]
+puts "Amazon"
+for contact in amazon.contacts
+    puts"#{contact.first_name} #{contact.last_name} #{contact.email}"
+end
+
+tesla = Company.where({name: "Tesla"})[0]
+puts "Tesla"
+for contact in tesla.contacts
+    puts"#{contact.first_name} #{contact.last_name} #{contact.email}"
+end
 # ---------------------------------
 # Contacts: 4
 # Andy Jassy - andy@amazon.com - Amazon.com, Inc.
